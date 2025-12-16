3f10d327c62675dc12839cd7ea48d17ef395dd59
Yuriy Izbash <izbash91@gmail.com>
2025-12-16 16:46:56 +0100

    Added Assets

---
+++ DropItLikeIt’sEgg/DropItLikeItIsEggApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropItLikeItIsEggApp.swift
+//  DropItLikeIt’sEgg
+//
+//  Created by yuriy on 16. 12. 25.
+//
+
+import SwiftUI
+
+@main
+struct DropItLikeItIsEggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
