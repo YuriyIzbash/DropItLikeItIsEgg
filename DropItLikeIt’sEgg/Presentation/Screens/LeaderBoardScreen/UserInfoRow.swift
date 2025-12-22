790b976488c871acfdc6a37be0bfae6e99533591
Yuriy Izbash <izbash91@gmail.com>
2025-12-22 11:56:26 +0100

    Fixed LeaderBoard Sizing Adoptation

--- DropItLikeIt’sEgg/Presentation/Screens/LeaderBoardScreen/UserInfoRow.swift
+++ DropItLikeIt’sEgg/Presentation/Screens/LeaderBoardScreen/UserInfoRow.swift
@@ -16,14 +16,14 @@ struct UserInfoRow: View {
             RoundedRectangle(cornerRadius: 8, style: .continuous)
                 .fill(Color.appPink)
             
-            HStack(spacing: 12) {
+            HStack(spacing: 8) {
                 Text(username.isEmpty ? "USERNAME" : username)
-                    .customFont(size: 16)
+                    .customFont(size: 14)
                     .lineLimit(1)
                     .frame(maxWidth: .infinity, alignment: .leading)
                 
                 Text("\(score)")
-                    .customFont(size: 16)
+                    .customFont(size: 12)
             }
             .padding(.horizontal, 16)
         }
