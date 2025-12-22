570c2cb13119460939c2be6a47946420f86a2429
Yuriy Izbash <izbash91@gmail.com>
2025-12-22 11:46:15 +0100

    Added How To Play Text

--- DropItLikeIt’sEgg/Presentation/Views/InfoView.swift
+++ DropItLikeIt’sEgg/Presentation/Views/InfoView.swift
@@ -26,8 +26,21 @@ struct InfoView: View {
                             Text("HOW TO PLAY")
                                 .customFont(size: 24)
                             
-                            Text("TEXT")
-                                .customFont(size: 12)
+                            Text("""
+                                1. 
+                                Catch falling eggs in your basket by moving left and right. 
+                                
+                                2. 
+                                Each caught egg earns you 10 points, but missing an egg costs you 100 points!
+                                
+                                3. 
+                                Grab coins for bonus points (20, 30, or 50 each). 
+                                
+                                4. 
+                                Catch all the eggs to win the level!
+                                """)
+                                .customFont(size: 16)
+                                .multilineTextAlignment(.center)
                                 .frame(maxHeight: .infinity, alignment: .center)
                         }
                             .padding(16),
